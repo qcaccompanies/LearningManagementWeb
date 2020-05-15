2150540933d7e67a861d16992510b24d86e3e68b
Yokebeth Denna Leksanti <dennaleksanti@gmail.com>
2020-05-15 15:52:08 +0700

    User Logout

---
+++ Test Suites/Denna/User - Logout.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User - Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c7731518-05d2-489a-8e16-d1c7a6de573d</testSuiteGuid>
+   <testCaseLink>
+      <guid>c41dce73-5bd6-41b5-a858-e2f6d181ad67</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Denna/User - Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
