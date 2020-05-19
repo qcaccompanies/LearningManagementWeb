ca56a9c90f65c00eceb66be038ae7de69498801d
geraldakensadhani <geraldakensadhani@yahoo.com>
2020-05-20 03:41:52 +0700

    update alda 20 mei

---
+++ Object Repository/Alda/Admin - Manage FAQ/Page_Dashboard Admin/btn_pretest_dan_posttest.rs
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_pretest_dan_posttest</name>
+   <tag></tag>
+   <elementGuidId>d3d0b234-c4b3-4a2c-81f0-8e3c71dc1f2c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body[@id='ng-app']/app-root/app-page-outlet/app-page-dashboardadmin/div/main/div/aside/app-partial-leftnavadmin/section/ul/li[3]/div/ul/li/a/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>anchor-caption</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Pre-Test &amp; Post-Test</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ng-app&quot;)/app-root[1]/app-page-outlet[@class=&quot;ng-star-inserted&quot;]/app-page-dashboardadmin[@class=&quot;ng-star-inserted&quot;]/div[@class=&quot;app-page container left-panel-collapsed-container slide-in-left-panel-container left-panel-container-md-10 left-panel-container-sm-10 left-panel-container-xs-2&quot;]/main[@class=&quot;app-content clearfix&quot;]/div[@class=&quot;row app-content-row clearfix&quot;]/aside[@class=&quot;app-left-panel left-panel-collapsed slide-in col-md-2 col-sm-2 col-xs-10&quot;]/app-partial-leftnavadmin[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;app-partial clearfix&quot;]/ul[@class=&quot;ng-star-inserted nav app-nav NavAdmin nav-pills nav-stacked&quot;]/li[@class=&quot;ng-star-inserted app-nav-item navAdminItem active&quot;]/div[@class=&quot;dropdown app-menu open&quot;]/ul[@class=&quot;icon-position-left dropdown-menu undefined animated wmSlideInDown ng-star-inserted&quot;]/li[@class=&quot;ng-star-inserted app-menu-item&quot;]/a[1]/span[@class=&quot;anchor-caption&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//body[@id='ng-app']/app-root/app-page-outlet/app-page-dashboardadmin/div/main/div/aside/app-partial-leftnavadmin/section/ul/li[3]/div/ul/li/a/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Manage Test'])[2]/following::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Manage Test'])[1]/following::span[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Quiz'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Manage FAQ'])[1]/preceding::span[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Pre-Test &amp; Post-Test']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/ul/li/a/span</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Alda/Admin_Manage FAQ - Search.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Admin_Manage FAQ - Search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>41716ab6-f7c1-408b-869c-81141ee793f7</testSuiteGuid>
+   <testCaseLink>
+      <guid>02c6685d-ebcd-47ec-9c2c-6fac2b889617</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Alda/Admin - Manage FAQ/Search</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>abd0b597-2616-4165-be54-d1e79ed77ad6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Alda/Admin_Manage FAQ - Search</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>abd0b597-2616-4165-be54-d1e79ed77ad6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>var_search</value>
+         <variableId>a71546f3-e5e6-4509-88ba-48395ab1b1fc</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>abd0b597-2616-4165-be54-d1e79ed77ad6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expected_output</value>
+         <variableId>8d6f0571-0992-4431-8174-1538a0e9f4c5</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
